7b9b6cfc180d74a3d07490ee37e8a7353effc3ce
Pjoter <pduczek9c@wp.pl>
2023-04-03 18:23:22 +0200

    2 xd

--- app/src/main/java/pl/hobbici/plosedzialalj/MainActivity.kt
+++ app/src/main/java/pl/hobbici/plosedzialalj/MainActivity.kt
@@ -2,7 +2,7 @@ package pl.hobbici.plosedzialalj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//xdddd
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
